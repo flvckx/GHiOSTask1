da8208a018f317e8f48be2428d58f0a0a57e4b75
flvckx <eqwem@ukr.net>
2015-11-07 22:19:11 +0200

    touches

---
+++ Hypnosyster/Hypnosyster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosyster
+//
+//  Created by Anatoliy on 11/7/15.
+//  Copyright © 2015 mobex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
